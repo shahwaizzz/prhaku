d6910c1273d5fe3334aa94bdc42d5b23a7264dc1
Shahwaiz Naveed <shahwaiznaveed46@gmail.com>
2022-05-25 04:56:31 -0700

    Ebook module

--- client/src/App.js
+++ client/src/App.js
@@ -1,134 +1,141 @@
-import React,{useState} from 'react';
-import './App.css';
-import {Container} from 'react-bootstrap';
-import Topbar from './components/Topbar';
-import Navbar from './components/Navbar';
-import SliderImage from './components/SliderImage';
-import Announcement from './components/Announcement';
-import QuickLinks from './components/QuickLinks'; 
-import FeaturedVideos from './components/FeaturedVideos';
-import Footer from './components/Footer';
-import NotesSlider from './components/NotesSlider';
-import MainSection from './pages/notes/MainSection';
-import CNSlider from './pages/ClassNotes/CNSlider';
-import CNMain from './pages/ClassNotes/CNMain';
-import PPMain from './pages/ppapers/PPMain';
-import Comments from './components/Comments';
-import BSlider from './pages/books/BSlider';
-import EBMain from './pages/books/EBMain';
-import TMain from './pages/test/TMain';
-import News from './pages/news/News';
-import NewsDetails from './pages/news/NewsDetails';
-import CNSlide from './components/CNSlide';
-import CNotes from './pages/notes/CNotes';
-import Papers from './pages/papers/Papers';
-import TSeries from './pages/papers/TSeries';
-import SScheme from './pages/study_scheme/SScheme';
-import GPapers from './pages/papers/GPapers';
-import PPapers from './pages/papers/PPapers';
-import RNavbar from './components/RNavbar';
-import {Routes,Route} from 'react-router-dom';
-import ClassesPage from './pages/admin/ClassesPage';
-import SubjectsPage from './pages/admin/SubjectsPage';
-import NotesPage from './pages/admin/NotesPage';
-import PapersPage from './pages/admin/PapersPage';
-import SSchemesPage from './pages/admin/SSchemesPage';
-import EBooksPage from './pages/admin/EBooksPage';
-import AdminLogin from './pages/admin/AdminLogin';
-
+import React, { useState } from "react";
+import "./App.css";
+import { Container } from "react-bootstrap";
+import Topbar from "./components/Topbar";
+import Navbar from "./components/Navbar";
+import SliderImage from "./components/SliderImage";
+import Announcement from "./components/Announcement";
+import QuickLinks from "./components/QuickLinks";
+import FeaturedVideos from "./components/FeaturedVideos";
+import Footer from "./components/Footer";
+import NotesSlider from "./components/NotesSlider";
+import MainSection from "./pages/notes/MainSection";
+import CNSlider from "./pages/ClassNotes/CNSlider";
+import CNMain from "./pages/ClassNotes/CNMain";
+import PPMain from "./pages/ppapers/PPMain";
+import Comments from "./components/Comments";
+import BSlider from "./pages/books/BSlider";
+import EBMain from "./pages/books/EBMain";
+import TMain from "./pages/test/TMain";
+import News from "./pages/news/News";
+import NewsDetails from "./pages/news/NewsDetails";
+import CNSlide from "./components/CNSlide";
+import CNotes from "./pages/notes/CNotes";
+import Papers from "./pages/papers/Papers";
+import TSeries from "./pages/papers/TSeries";
+import SScheme from "./pages/study_scheme/SScheme";
+import GPapers from "./pages/papers/GPapers";
+import PPapers from "./pages/papers/PPapers";
+import RNavbar from "./components/RNavbar";
+import { Routes, Route } from "react-router-dom";
+import ClassesPage from "./pages/admin/ClassesPage";
+import SubjectsPage from "./pages/admin/SubjectsPage";
+import NotesPage from "./pages/admin/NotesPage";
+import PapersPage from "./pages/admin/PapersPage";
+import SSchemesPage from "./pages/admin/SSchemesPage";
+import EBooksPage from "./pages/admin/EBooksPage";
+import AdminLogin from "./pages/admin/AdminLogin";
 
 function App() {
   const [nav, setNav] = useState(0);
   const [paper, setPaper] = useState(9);
   return (
     <Routes>
-     <Route path="/" element={
-        <div>
-        <Topbar />
-        {/* <Navbar setNav={setNav} /> */}
-        <RNavbar className="d-md-none d-block" setNav={setNav} />
-        <Navbar className="d-md-block d-none" />
-        {
-          nav === 0?(
-            <>
-              <SliderImage />
-              <Announcement />
-              <Container className='py-5'>
-                <div className='advert'>
-                  <h1>Advert</h1>
-                </div>
-              </Container>
-              <QuickLinks />
-              <Container className='py-5'>
-                <div className='advert'>
-                  <h1>Advert</h1>
-                </div>
-              </Container>
-              <FeaturedVideos />
-            </>
-          ):nav === 1?(
-            <>
-              <NotesSlider />
-              <MainSection />
-            </>
-          ):nav === 3?(
-            <>
-              <CNSlider />
-              <CNMain />
-            </>
-
-          ):nav === 4?(
-            <>
-              <PPMain />
-              <Comments />
-            </>
-          ):nav === 5?(
-            <>
-              <BSlider />
-              <EBMain />
-            </>
-          ):nav === 6?(
-            <>
-              <TMain />
-              <Comments />
-            </>
-          ):nav === 7?(
-            <>
-              <News />
-            </>
-          ):nav === 8?(
-            <>
-              <NewsDetails />
-              <Comments />
-            </>
-          ):nav === 9?(
-            <>
-              <CNSlide />
-              <CNotes />
-            </>
-          ):nav === 10?(
-            <>
-              <Papers setPaper={setPaper} paper={paper} />
-              {paper === 0?(<TSeries />):paper === 1? (<GPapers />):(<PPapers />)}
-              <Comments className='mt-5' />         
-            </>
-          ):nav === 11?(
-            <>
-              <SScheme />
-            </>
-          ):''
+      <Route
+        path='/'
+        element={
+          <div>
+            <Topbar />
+            {/* <Navbar setNav={setNav} /> */}
+            <RNavbar className='d-md-none d-block' setNav={setNav} />
+            <Navbar className='d-md-block d-none' />
+            {nav === 0 ? (
+              <>
+                <SliderImage />
+                <Announcement />
+                <Container className='py-5'>
+                  <div className='advert'>
+                    <h1>Advert</h1>
+                  </div>
+                </Container>
+                <QuickLinks />
+                <Container className='py-5'>
+                  <div className='advert'>
+                    <h1>Advert</h1>
+                  </div>
+                </Container>
+                <FeaturedVideos />
+              </>
+            ) : nav === 1 ? (
+              <>
+                <NotesSlider />
+                <MainSection />
+              </>
+            ) : nav === 3 ? (
+              <>
+                <CNSlider />
+                <CNMain />
+              </>
+            ) : nav === 4 ? (
+              <>
+                <PPMain />
+                <Comments />
+              </>
+            ) : nav === 5 ? (
+              <>
+                <BSlider />
+                <EBMain />
+              </>
+            ) : nav === 6 ? (
+              <>
+                <TMain />
+                <Comments />
+              </>
+            ) : nav === 7 ? (
+              <>
+                <News />
+              </>
+            ) : nav === 8 ? (
+              <>
+                <NewsDetails />
+                <Comments />
+              </>
+            ) : nav === 9 ? (
+              <>
+                <CNSlide />
+                <CNotes />
+              </>
+            ) : nav === 10 ? (
+              <>
+                <Papers setPaper={setPaper} paper={paper} />
+                {paper === 0 ? (
+                  <TSeries />
+                ) : paper === 1 ? (
+                  <GPapers />
+                ) : (
+                  <PPapers />
+                )}
+                <Comments className='mt-5' />
+              </>
+            ) : nav === 11 ? (
+              <>
+                <SScheme />
+              </>
+            ) : (
+              ""
+            )}
+            <Footer />
+          </div>
         }
-        <Footer />
-        </div>
-      } /> 
-      <Route path="/adminlogin" element={<AdminLogin />} />
-      <Route path="/admin" element={<ClassesPage />} />
-      <Route path="/admin/subjects" element={<SubjectsPage />} />
-      <Route path="/admin/notes" element={<NotesPage />} />
-      <Route path="/admin/papers" element={<PapersPage />} />
-      <Route path="/admin/studyschemes" element={<SSchemesPage />} />
-      <Route path="/admin/ebooks" element={<EBooksPage />} />
-      <Route path="/admin/classes" element={<ClassesPage />} />
+      />
+      <Route path='/adminlogin' element={<AdminLogin />} />
+      <Route path='/admin' element={<ClassesPage />} />
+      <Route path='/admin/subjects' element={<SubjectsPage />} />
+      <Route path='/admin/notes' element={<NotesPage />} />
+      <Route path='/admin/papers' element={<PapersPage />} />
+      <Route path='/admin/studyschemes' element={<SSchemesPage />} />
+      <Route path='/admin/ebooks' element={<EBooksPage />} />
+      <Route path='/admin/classes' element={<ClassesPage />} />
     </Routes>
   );
 }
--- client/src/pages/admin/EBooks.js
+++ client/src/pages/admin/EBooks.js
@@ -1,33 +1,33 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Button from  'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal'
-import Form from 'react-bootstrap/Form'
-import {useForm} from 'react-hook-form'
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
 
 const EBooks = () => {
   const [show, setShow] = useState(false);
-  const [show1, setShow1] = useState(false);  
+  const [show1, setShow1] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [img, setImg] = useState(null);
   const [doc, setDoc] = useState(null);
 
-  const {register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
 
   // useEffect(() => {
   //   axios.get("http://localhost:5000/api/categories").then((response) => {
   //     setCat(response.data);
   //   }).catch((error) => {
   //     console.log(error);
-  //   })    
+  //   })
   // },[])
   // useEffect(() => {
   //   axios.get("http://localhost:5000/api/products").then((response) => {
   //     setData(response.data);
   //   }).catch((error) => {
   //     console.log(error);
-  //   })    
+  //   })
   // },[])
   const [product, setProduct] = useState({
     title: "",
@@ -43,7 +43,6 @@ const EBooks = () => {
   const handleInput = (e) => {
     // name = e.target.name;
     // value = e.target.value;
-
     // setProduct({ ...product, [name]: value });
   };
   const onChange = (e) => {
@@ -51,7 +50,7 @@ const EBooks = () => {
   };
 
   const handleSubmit3 = (e) => {
-    alert("Request send")
+    alert("Request send");
     e.preventDefault();
     console.log("hello world");
     const formData = new FormData();
@@ -60,8 +59,7 @@ const EBooks = () => {
     formData.append("ebookImage", product.image);
     formData.append("ebookDoc", product.bookfile);
     console.log("FormData : ", formData);
-
-  }
+  };
 
   const handleSubmit1 = async (e) => {
     // alert("Request send")
@@ -76,7 +74,6 @@ const EBooks = () => {
     // formData.append("color", product.color);
     // formData.append("price", product.price);
     // formData.append("quantity", product.quantity);
-
     // axios
     //   .post("http://localhost:5000/api/products/add", formData, {
     //     headers: {
@@ -93,67 +90,70 @@ const EBooks = () => {
     //     alert("error in sending request");
     //   });
   };
-  function delFun(idd){
-  //   alert(`${baseURL}/`+idd);
-  //   axios.delete("http://localhost:5000/api/products/"+idd, {
-  //     headers: { 
-  //       "token": 'Bearer '+auth,
-  //     },
-  //   }).then((response) => {
-  //     console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-  //     console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-  //     console.log(response);
-  //     console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-  //     alert("Product deleted Successfully");
-  //   }).catch((error) => {
-  //     console.log(error);
-  //   })
-  // }
-  // function updateFun(element){
-  //   setProduct({...element});
-  //   console.log("UPDATE PRODUCT UPDATE PRODUCT UPDATE PRODUCT");
-  //   console.log("product : ",product);
-  // }
-  // function handleSubmit2(e){
-  //   e.preventDefault();
-  //   alert('form submittedd');
-  //   console.log("updated product object value");
-  //   console.log("U product : ", product);
-  //   const formData = new FormData();
-  //   formData.append("productimg", product.img);
-  //   formData.append("title", product.title);
-  //   formData.append("desc", product.desc);
-  //   formData.append("category", product.category);
-  //   formData.append("size", product.size);
-  //   formData.append("color", product.color);
-  //   formData.append("price", product.price);
-  //   formData.append("quantity", product.quantity);
-
-  //   axios
-  //     .put(baseURL+"/"+product._id, formData, {
-  //       headers: {
-  //         "Content-Type": "multipart/form-data",
-  //         "token": `Bearer ${auth}`,
-  //       },
-  //     })
-  //     .then((res) => {
-  //       console.log(res);
-  //       alert("Product Updated successfully");
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //       alert("error in sending request");
-  //     });
+  function delFun(idd) {
+    //   alert(`${baseURL}/`+idd);
+    //   axios.delete("http://localhost:5000/api/products/"+idd, {
+    //     headers: {
+    //       "token": 'Bearer '+auth,
+    //     },
+    //   }).then((response) => {
+    //     console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+    //     console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+    //     console.log(response);
+    //     console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+    //     alert("Product deleted Successfully");
+    //   }).catch((error) => {
+    //     console.log(error);
+    //   })
+    // }
+    // function updateFun(element){
+    //   setProduct({...element});
+    //   console.log("UPDATE PRODUCT UPDATE PRODUCT UPDATE PRODUCT");
+    //   console.log("product : ",product);
+    // }
+    // function handleSubmit2(e){
+    //   e.preventDefault();
+    //   alert('form submittedd');
+    //   console.log("updated product object value");
+    //   console.log("U product : ", product);
+    //   const formData = new FormData();
+    //   formData.append("productimg", product.img);
+    //   formData.append("title", product.title);
+    //   formData.append("desc", product.desc);
+    //   formData.append("category", product.category);
+    //   formData.append("size", product.size);
+    //   formData.append("color", product.color);
+    //   formData.append("price", product.price);
+    //   formData.append("quantity", product.quantity);
+    //   axios
+    //     .put(baseURL+"/"+product._id, formData, {
+    //       headers: {
+    //         "Content-Type": "multipart/form-data",
+    //         "token": `Bearer ${auth}`,
+    //       },
+    //     })
+    //     .then((res) => {
+    //       console.log(res);
+    //       alert("Product Updated successfully");
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //       alert("error in sending request");
+    //     });
   }
 
-  const onSubmit = (data) => {
-    console.log(data);
-    const ob = {title: data.title, category: data.category, ebookImage: img, ebookDoc: doc}
-    console.log(ob)
-    console.log('img : ',img);
-    console.log('doc : ',doc);
+  const onSubmit = (ebook) => {
+    console.log(ebook);
+    const formData = new FormData();
+    formData.append("title", ebook.title);
+    formData.append("category", ebook.category);
+    formData.append("ebookImage", img);
+    formData.append("ebookDoc", doc);
+    console.log("FormData : ", formData);
+    console.log("img : ", img);
+    console.log("doc : ", doc);
     axios
-      .post("http://localhost:5000/admin/create-ebook", {title: data.title, category: data.category, ebookImage: img, ebookDoc: doc})
+      .post("http://localhost:5000/admin/create-ebook", formData)
       .then((res) => {
         console.log(res);
         alert("EBook added successfully");
@@ -162,20 +162,23 @@ const EBooks = () => {
         console.log(err);
         alert("error in sending request");
       });
-  }
-
+  };
 
   return (
     <>
       <div className='recent-sales box' style={{ width: "100%" }}>
         <div className='title'>
           E-Books
-          <button onClick={handleShow} className='add-pbtn btn btn-success' style={{float: 'right'}}>
+          <button
+            onClick={handleShow}
+            className='add-pbtn btn btn-success'
+            style={{ float: "right" }}
+          >
             Add E-Book
           </button>
         </div>
         <div className='sales-details'>
-          <table className='class-table mt-3' style={{width: '100%'}}> 
+          <table className='class-table mt-3' style={{ width: "100%" }}>
             <thead>
               <tr>
                 <th>Sr</th>
@@ -188,41 +191,77 @@ const EBooks = () => {
               </tr>
             </thead>
             <tbody>
-              <tr className="mt-2">
+              <tr className='mt-2'>
                 <td>1</td>
                 <td>Class One</td>
                 <td>Cat2</td>
                 <td>Image</td>
                 <td>file1</td>
-                <td><button className="btn mt-2 btn-primary px-4" onClick={() => setShow1(true)}>Edit</button></td>
-                <td><button className="btn mt-2 btn-danger">Delete</button></td>
+                <td>
+                  <button
+                    className='btn mt-2 btn-primary px-4'
+                    onClick={() => setShow1(true)}
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button className='btn mt-2 btn-danger'>Delete</button>
+                </td>
               </tr>
-              <tr className="mt-2">
+              <tr className='mt-2'>
                 <td>2</td>
                 <td>Class One</td>
                 <td>cat3</td>
                 <td>Image</td>
                 <td>file23</td>
-                <td><button className="btn mt-2 btn-primary px-4" onClick={() => setShow1(true)}>Edit</button></td>
-                <td><button className="btn mt-2 btn-danger">Delete</button></td>
+                <td>
+                  <button
+                    className='btn mt-2 btn-primary px-4'
+                    onClick={() => setShow1(true)}
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button className='btn mt-2 btn-danger'>Delete</button>
+                </td>
               </tr>
-              <tr className="mt-2">
+              <tr className='mt-2'>
                 <td>3</td>
                 <td>Class One</td>
                 <td>cat2</td>
                 <td>Image</td>
                 <td>Image</td>
-                <td><button className="btn mt-2 btn-primary px-4" onClick={() => setShow1(true)}>Edit</button></td>
-                <td><button className="btn mt-2 btn-danger">Delete</button></td>
+                <td>
+                  <button
+                    className='btn mt-2 btn-primary px-4'
+                    onClick={() => setShow1(true)}
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button className='btn mt-2 btn-danger'>Delete</button>
+                </td>
               </tr>
-              <tr className="mt-2">
+              <tr className='mt-2'>
                 <td>4</td>
                 <td>Class One</td>
                 <td>cat6</td>
                 <td>Image</td>
                 <td>Image</td>
-                <td><button className="btn mt-2 btn-primary px-4" onClick={() => setShow1(true)}>Edit</button></td>
-                <td><button className="btn mt-2 btn-danger">Delete</button></td>
+                <td>
+                  <button
+                    className='btn mt-2 btn-primary px-4'
+                    onClick={() => setShow1(true)}
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button className='btn mt-2 btn-danger'>Delete</button>
+                </td>
               </tr>
             </tbody>
           </table>
@@ -231,69 +270,91 @@ const EBooks = () => {
           <a href='#'>See All</a>
         </div>
       </div>
-          {/* modal for adding new item */}
+      {/* modal for adding new item */}
 
-          <Modal show={show} onHide={handleClose}>
-              <Form encType="multipart/form-data" onSubmit={handleSubmit(onSubmit)}>
-            <Modal.Header closeButton>
-              <Modal.Title className="fw-700">Add E-Book</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form.Group className="mb-3" controlId="formGroupEmail">
-                  <Form.Label>E-Book Title</Form.Label>
-                  <Form.Control type="text" {...register("title")} placeholder="Enter E-Book Title" required />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroupCategory">
-                  <Form.Label>E-Book Category</Form.Label>
-                  <Form.Control type="text" {...register("category")} placeholder="Enter E-Book Title" required />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroupPassword" >
-                  <Form.Label>Image</Form.Label>
-                  <Form.Control type="file" {...register("ebookImage")} name="img" onChange={(e) => setImg(e.target.files[0])} placeholder="Select An Image" required />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroupPassword">
-                  <Form.Label>File</Form.Label>
-                  <Form.Control type="file" {...register("ebookDoc")} name="doc" onChange={(e) => setDoc(e.target.files[0])} placeholder="Select An Image" required />
-                </Form.Group>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Modal.Footer>
-              </Form>
-          </Modal>
+      <Modal show={show} onHide={handleClose}>
+        <Form encType='multipart/form-data' onSubmit={handleSubmit(onSubmit)}>
+          <Modal.Header closeButton>
+            <Modal.Title className='fw-700'>Add E-Book</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Group className='mb-3' controlId='formGroupEmail'>
+              <Form.Label>E-Book Title</Form.Label>
+              <Form.Control
+                type='text'
+                {...register("title")}
+                placeholder='Enter E-Book Title'
+                required
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formGroupCategory'>
+              <Form.Label>E-Book Category</Form.Label>
+              <Form.Control
+                type='text'
+                {...register("category")}
+                placeholder='Enter E-Book Title'
+                required
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formGroupPassword'>
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type='file'
+                {...register("ebookImage")}
+                name='img'
+                onChange={(e) => setImg(e.target.files[0])}
+                placeholder='Select An Image'
+                required
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formGroupPassword'>
+              <Form.Label>File</Form.Label>
+              <Form.Control
+                type='file'
+                {...register("ebookDoc")}
+                name='doc'
+                onChange={(e) => setDoc(e.target.files[0])}
+                placeholder='Select An Image'
+                required
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant='secondary' onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant='primary' type='submit'>
+              Submit
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
 
-          {/* Model for updating item */}
+      {/* Model for updating item */}
 
-          <Modal show={show1} onHide={() => setShow1(false)}>
-            <Modal.Header closeButton>
-              <Modal.Title className="fw-700">Edit Class</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form encType="multipart/form-data">
-                <Form.Group className="mb-3" controlId="formGroupEmail">
-                  <Form.Label>Class Name</Form.Label>
-                  <Form.Control type="text" placeholder="Enter Class Name" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroupPassword">
-                  <Form.Label>Image</Form.Label>
-                  <Form.Control type="file" placeholder="Select An Image" />
-                </Form.Group>
-              </Form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={() => setShow1(false)}>
-                Close
-              </Button>
-              <Button variant="primary">
-                Submit
-              </Button>
-            </Modal.Footer>
-          </Modal>
+      <Modal show={show1} onHide={() => setShow1(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title className='fw-700'>Edit Class</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form encType='multipart/form-data'>
+            <Form.Group className='mb-3' controlId='formGroupEmail'>
+              <Form.Label>Class Name</Form.Label>
+              <Form.Control type='text' placeholder='Enter Class Name' />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formGroupPassword'>
+              <Form.Label>Image</Form.Label>
+              <Form.Control type='file' placeholder='Select An Image' />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={() => setShow1(false)}>
+            Close
+          </Button>
+          <Button variant='primary'>Submit</Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
